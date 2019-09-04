cc2ed4d7359795d705679989d24dac0aa986850d
mena denis <denis.menacai@netvisioncorporation.com>
2019-09-04 13:52:32 +0300

    as found

--- ScrapperBursa/Form1.Designer.cs
+++ ScrapperBursa/Form1.Designer.cs
@@ -28,13 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1078, 312);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(235, 761);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(404, 760);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(563, 760);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.webBrowser2.Location = new System.Drawing.Point(0, 312);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.Size = new System.Drawing.Size(1078, 391);
+            this.webBrowser2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1078, 805);
+            this.Controls.Add(this.webBrowser2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.WebBrowser webBrowser2;
     }
 }
 
--- ScrapperBursa/Form1.cs
+++ ScrapperBursa/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,119 @@ namespace ScrapperBursa
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //webBrowser1.Navigate("https://www.google.ro/");            
+            webBrowser1.Navigate("https://www.bursatransport.com/login");
+            //label1.Text = webBrowser1.Version.Major.ToString();
+            //webBrowser1.Document.GetElementById("Login_username").InnerHtml = "sesconsulting";
+            //webBrowser1.Document.GetElementById("Login_password").InnerHtml = "romtas";
+
+            //webBrowser1.Document.GetElementsByTagName("yt1").InvokeMember("click");
+            //Login_username
+            //Login_password
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var inputEl = webBrowser1.Document.GetElementsByTagName("input");
+            foreach (HtmlElement i in inputEl)
+            {
+                if (i.GetAttribute("name").Equals("Login[username]"))
+                {
+                    i.InnerText = "sesconsulting";
+                }
+                if (i.GetAttribute("name").Equals("Login[password]"))
+                {
+                    i.Focus();
+                    i.InnerText = "romtas";
+                }
+            }
+
+            var buttonEl = webBrowser1.Document.GetElementsByTagName("button");
+            foreach (HtmlElement b in buttonEl)
+            {
+                if (b.GetAttribute("name").Equals("yt1"))
+                    b.InvokeMember("click");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.bursatransport.com/memberlist?doSearch=1&privateexchange_id=&MemberSearch2%5Btext%5D=&MemberSearch2%5Bdescription%5D=&MemberSearch2%5Bactivity%5D=Caraus&MemberSearch2%5BactiveTransportLicence%5D=0&MemberSearch2%5BactiveForwardingLicence%5D=0&MemberSearch2%5BactiveCompanies%5D=0&source_id_txt=&searchType=1&yt0=&range=0&page=1");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var linkEl = webBrowser1.Document.GetElementsByTagName("a");
+            string compId = string.Empty;
+            string compName = string.Empty;
+            string url = string.Empty;
+            foreach (HtmlElement a in linkEl)
+            {
+                if (a.GetAttribute("className").Equals("companyProfileUrl"))
+                {
+                    compName = a.InnerText;
+                    compId = a.Parent.Parent.Children[0].InnerText;
+                    Console.WriteLine(compId);
+                    Console.WriteLine(a.InnerText);
+                    //https://www.bursatransport.com/account/default/profile/company_id/215453/reftype/300
+                    url = "https://www.bursatransport.com/account/default/profile/company_id/" + compId + "/reftype/300";
+                    webBrowser2.Navigate(url);
+                    webBrowser2.ScriptErrorsSuppressed = true;
+                    //Thread.Sleep(10);
+                    //Application.DoEvents();
+                    while (webBrowser2.ReadyState != WebBrowserReadyState.Complete)
+                    {
+                        //RTBAction("Current Action: Loading " + Category + " | " + Subcategory + " | " + ItemLink + " (" + Browsersw.Elapsed.Seconds.ToString() + " sec)");
+                        //UpdatelabelElapsed("Elapsed Time: " + sw.Elapsed.Days + " day(s), " + sw.Elapsed.Hours + " hours, " + sw.Elapsed.Minutes + " min");
+                        Thread.Sleep(5);
+                        Application.DoEvents();
+                    }
+
+                    var dataEl = webBrowser2.Document.GetElementsByTagName("td");
+                    string cif = string.Empty;
+                    string anulInfiintarii = string.Empty;
+                    string sediulSocial = string.Empty;
+                    string administrator = string.Empty;
+                    string dataInscrierii = string.Empty;
+
+                    foreach (HtmlElement d in dataEl)
+                    {
+                        if (d.GetAttribute("className").Equals("companyLabel"))
+                        {
+                            switch (d.InnerText)
+                            {
+                                case "C.I.F.:":
+                                    cif = d.NextSibling.InnerText;
+                                    break;
+                                case "Anul infiintarii:":
+                                    anulInfiintarii = d.NextSibling.InnerText;
+                                    break;
+                                case "Sediu social:":
+                                    sediulSocial = d.NextSibling.InnerText;
+                                    break;
+                                case "Administrator:":
+                                    administrator = d.NextSibling.InnerText;
+                                    break;
+                                case "Inscris in BursaTransport:":
+                                    dataInscrierii = d.NextSibling.InnerText;
+                                    break;
+                                default: break;
+                            }
+                        }
+                    }
+
+                    System.IO.StreamWriter file = new System.IO.StreamWriter("output.csv", true); //append!
+                    file.WriteLine("" + compId + "" + ";" + "" + compName + "" + ";" + "" + cif + "" + ";" + anulInfiintarii + "" + ";" +
+                        sediulSocial + "" + ";" + administrator + "" + ";" + dataInscrierii + "");
+                    file.Flush();
+                    file.Close();
+                    Application.DoEvents();
+                }
+            }
         }
     }
 }
